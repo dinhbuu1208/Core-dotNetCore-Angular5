5c0e90ad1c583979496df9e6d0604c9fbcf25ec3
dinhbuu1208@gmail.com <dinhbuu1208@gmail.com>
2018-10-24 22:09:46 +0700

    update 24/10/2018

---
+++ TestMakerFreeWebApp/TestMakerFree/TestMakerFreeWebApp/Controllers/QuizController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TestMakerFreeWebApp.ViewModel;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TestMakerFreeWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class QuizController : Controller
+    {
+        // GET: api/quiz/latest
+        [HttpGet("Latest/{num}")]
+        public IActionResult Latest(int num = 10)
+        {
+            var sampleQuizzes = new List<QuizViewModel>();
+
+            //add the first sample quiz
+            sampleQuizzes.Add(new QuizViewModel()
+            {
+                Id = 1 ,
+                Title = "Which Shingeki No Kyojin character are you?",
+                Description = "Anime-related personality test",
+                CreatedDate = DateTime.Now,
+                LastModifiedDate = DateTime.Now
+            });
+
+            //add a bunch of the other sample quizzes
+            for(int i = 2; i <= num; i++)
+            {
+                sampleQuizzes.Add(new QuizViewModel()
+                {
+                    Id = i,
+                    Title = String.Format("Sample Quiz {0}", i),
+                    Description = "This is a sample quiz",
+                    CreatedDate = DateTime.Now,
+                    LastModifiedDate = DateTime.Now
+                });
+            }
+
+            // output the result in JSON format
+            return new JsonResult(
+                    sampleQuizzes,
+                    new JsonSerializerSettings()
+                    {
+                        Formatting = Formatting.Indented
+                    }
+                );           
+        }      
+
+        /// <summary>
+        /// GET: api/quiz/ByTitle
+        /// Retrieves the {num} Quizzes sorted by Title (A to Z)
+        /// </summary>
+        /// <param name="num">the number of quizzes to retrieve</param>
+        /// <returns>{num} Quizzes sorted by Title</returns> 
+        [HttpGet("ByTitile/{num:int?}")]
+        public IActionResult ByTitle(int num = 10)
+        {
+            var sampleQuizzes = ((JsonResult)Latest(num)).Value as List<QuizViewModel>;
+
+            return new JsonResult(
+                    sampleQuizzes.OrderBy(t => t.Title),
+                    new JsonSerializerSettings()
+                    {
+                        Formatting = Formatting.Indented
+                    });
+        }
+
+        /// <summary>
+        /// GET: api/quiz/mostViewed
+        /// Retrieves the {num} random Quizzes
+        /// </summary>
+        /// <param name="num">the number of quizzes to retrieve</param>
+        /// <returns>{num} random Quizzes</returns> 
+        [HttpGet("Random/{num:int?}")]
+        public IActionResult Random(int num = 10)
+        {
+            var sampleQuizzes = ((JsonResult)Latest(num)).Value as List<QuizViewModel>;
+
+            return new JsonResult(
+                    sampleQuizzes.OrderBy(t => Guid.NewGuid()),
+                    new JsonSerializerSettings()
+                    {
+                        Formatting = Formatting.Indented
+                    }
+                );
+        }
+
+
+    }
+}
